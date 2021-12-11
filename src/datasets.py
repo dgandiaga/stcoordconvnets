617c7d13e3dcde4f2526509aa89b2f6d2958e29a
daniel.gandiaga <daniel.gandiaga@dive.tech>
2021-12-11 02:43:34 +0100

    Added birds dataset

--- src/datasets.py
+++ src/datasets.py
@@ -2,6 +2,7 @@ import logging
 from torchvision import datasets, transforms
 import torch
 from six.moves import urllib
+import math
 
 
 def get_dataset(name, root='datasets'):
@@ -51,10 +52,39 @@ def get_dataset(name, root='datasets'):
         val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
         test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
 
+    elif name == 'birds':
+
+        transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                         transforms.Resize([200, 200])])
+        dataset = BirdDataset(transform)
+
+        train_length = int(math.floor(len(dataset)*0.8))
+        val_length = int(math.floor(len(dataset) * 0.1))
+        test_length = len(dataset) - train_length - val_length
+
+        train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset,
+                                                                 [train_length, val_length, test_length],
+                                                                 generator=torch.Generator().manual_seed(42))
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=False, num_workers=4)
+        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=4)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
+
     else:
         logging.error(f'Please specify a valid dataset. Dataset specified: {name}')
 
-
-
-
     return train_loader, val_loader, test_loader
+
+# Class for the Bird dataset
+class BirdDataset(torch.utils.data.Dataset):
+    def __init__(self,  transform=None):
+        super(BirdDataset, self).__init__()
+        self.data = datasets.ImageFolder('datasets/CUB_200_2011',  transform)    # Create data from folder
+
+    def __getitem__(self, idx):
+        x, y = self.data[idx]
+        return x, y
+
+    def __len__(self):
+        return len(self.data)
